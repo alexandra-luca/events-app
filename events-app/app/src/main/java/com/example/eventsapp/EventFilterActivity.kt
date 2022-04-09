01d2cbde0b54ee4a49e60ec968fda5431d022a61
Alexandra Luca <alexandra.luca1611@gmail.com>
2022-04-09 22:28:00 +0300

    Create settings activity

---
+++ events-app/app/src/main/java/com/example/eventsapp/EventFilterActivity.kt
@@ -0,0 +1,26 @@
+package com.example.eventsapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+
+class EventFilterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.settings, SettingsFragment())
+                    .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.filter_preferences, rootKey)
+        }
+    }
+}
