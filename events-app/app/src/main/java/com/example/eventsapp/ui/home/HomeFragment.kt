9ddb8c5ced81b5225950d54218d2597e85f017f1
Alexandra Luca <alexandra.luca1611@gmail.com>
2022-04-13 21:10:26 +0300

    Create event http request

--- events-app/app/src/main/java/com/example/eventsapp/ui/home/HomeFragment.kt
+++ events-app/app/src/main/java/com/example/eventsapp/ui/home/HomeFragment.kt
@@ -1,17 +1,28 @@
 package com.example.eventsapp.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.beust.klaxon.Klaxon
+import com.example.eventsapp.EventFilterActivity
+import com.example.eventsapp.R
 import com.example.eventsapp.databinding.FragmentHomeBinding
+import okhttp3.*
+import java.io.IOException
+
+data class Event(val id: Int, val name: String, val type: String, val pricing: String);
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+
+    private var homeViewModel: HomeViewModel? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +33,14 @@ class HomeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
+        homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        homeViewModel!!.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
@@ -38,5 +49,41 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_filter).setOnClickListener {
+            val intent = Intent(context, EventFilterActivity::class.java);
+            startActivity(intent);
+        }
+
+        view.findViewById<Button>(R.id.button_refresh).setOnClickListener {
+            val eventsUrl = "https://my-json-server.typicode.com/alexandra-luca/events-app/events"
+
+            val client = OkHttpClient()
+            val request = Request.Builder().url(eventsUrl).build()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {}
+                override fun onResponse(call: Call, response: Response) {
+                    val responseString = response.body()?.string()!!;
+                    val events = Klaxon().parseArray<Event>(responseString)!!
+                    editText(view, events)
+                }
+            })
+        }
+    }
+
+    fun editText(view: View, events: List<Event>) {
+        requireActivity()!!.runOnUiThread(object : Runnable {
+            override fun run() {
+
+                val eventsTextView = view.findViewById<TextView>(R.id.eventsTextView)
+                eventsTextView.text = events.joinToString(", ") { event -> event.name }
+
+            }
+        })
     }
 }
--- events-app/app/src/main/java/com/example/eventsapp/ui/home/HomeViewModel.kt
+++ events-app/app/src/main/java/com/example/eventsapp/ui/home/HomeViewModel.kt
@@ -5,9 +5,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
+    private var _text = MutableLiveData<String>().apply {
         value = "This is events Fragment"
     }
+
     val text: LiveData<String> = _text
 }
