0481484cea1d0386180ec7b0041c3cab19741143
Alexandra Luca <alexandra.luca1611@gmail.com>
2022-05-11 18:04:17 +0300

    Create welcome notification

--- events-app/app/src/main/java/com/example/eventsapp/MainActivity.kt
+++ events-app/app/src/main/java/com/example/eventsapp/MainActivity.kt
@@ -7,6 +7,8 @@ import android.os.Bundle
 import android.content.Context
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -33,6 +35,34 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        createNotificationChannel()
+        var builder = NotificationCompat.Builder(this, "0")
+            .setSmallIcon(R.drawable.ic_baseline_add_alert_24)
+            .setContentTitle("Welcome back to EventsApp!")
+            .setContentText("Current login streak: 1")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(5, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("0", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
 //    private fun createNotificationChannel() {
